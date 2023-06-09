d557c97b83675aca82c7b6ec2b08e662f4920179
AL-Hareth <alharethturab@gmail.com>
2023-06-10 01:13:16 +0300

    This thing is actually working, don't touch it

--- src/routes/blogs/[blogId]/index.tsx
+++ src/routes/blogs/[blogId]/index.tsx
@@ -1,72 +1,61 @@
 import { component$ } from "@builder.io/qwik";
 import {
-  type StaticGenerateHandler,
-  useLocation,
-  type DocumentHead,
-  server$,
+    useLocation,
+    type DocumentHead,
+    server$,
 } from "@builder.io/qwik-city";
 import prisma from "~/lib/prisma";
 import { usePostData } from "../../layout";
 import getArabicDateFormat from "~/lib/getArabicDateFormat";
 
 export const getUserById = server$(async (userId: string) => {
-  return await prisma.user.findUnique({
-    where: {
-      id: userId,
-    },
-  });
+    return await prisma.user.findUnique({
+        where: {
+            id: userId,
+        },
+    });
 });
 
 export default component$(() => {
-  const { params } = useLocation();
-  const posts = usePostData();
-  const post = posts.value.find((post) => post.id === params.blogId);
-  return post ? (
-    <div class="w-3/5 mx-auto">
-      <h1 class="text-5xl text-center">{post.title}</h1>
-      {getUserById(post.userId).then(
-        (user) => user && <span>الكاتب: {user.name}</span>
-      )}
-      <div class="my-5">
-        {
-          //categories
-          post.catrgories.map((category) => {
-            return (
-              <span
-                key={category.id}
-                class="bg-primary text-white px-2 py-1 mx-1 rounded-lg"
-              >
-                {category.name}
-              </span>
-            );
-          })
-        }
-      </div>
-      <div
-        class="mt-4 flex flex-col items-stretch"
-        style={{ lineHeight: "1.8rem" }}
-      >
-        <p dangerouslySetInnerHTML={post.body}></p>
-      </div>
-      <span class="text-sm text-gray-500">
-        {post?.createdAt && getArabicDateFormat(post.createdAt)}
-      </span>
-    </div>
-  ) : null;
+    const { params } = useLocation();
+    const posts = usePostData();
+    const post = posts.value.find((post) => post.id === params.blogId);
+    return post ? (
+        <div class="w-3/5 mx-auto">
+            <h1 class="text-5xl text-center">{post.title}</h1>
+            {getUserById(post.userId).then(
+                (user) => user && <span>الكاتب: {user.name}</span>
+            )}
+            <div class="my-5">
+                {
+                    //categories
+                    post.catrgories.map((category) => {
+                        return (
+                            <span
+                                key={category.id}
+                                class="bg-primary text-white px-2 py-1 mx-1 rounded-lg"
+                            >
+                                {category.name}
+                            </span>
+                        );
+                    })
+                }
+            </div>
+            <div
+                class="mt-4 flex flex-col items-stretch"
+                style={{ lineHeight: "1.8rem" }}
+            >
+                <p dangerouslySetInnerHTML={post.body}></p>
+            </div>
+            <span class="text-sm text-gray-500">
+                {post?.createdAt && getArabicDateFormat(post.createdAt)}
+            </span>
+        </div>
+    ) : null;
 });
 
-export const onStaticGenerate: StaticGenerateHandler = async () => {
-  const posts = await prisma.post.findMany();
-
-  return {
-    params: posts.map((post) => ({
-      blogId: post.id,
-    })),
-  };
-};
-
 export const head: DocumentHead = ({ resolveValue, params }) => {
-  const posts = resolveValue(usePostData);
-  const title = posts.find((post) => post.id === params.blogId)?.title;
-  return { title };
+    const posts = resolveValue(usePostData);
+    const title = posts.find((post) => post.id === params.blogId)?.title;
+    return { title };
 };
--- src/routes/signin/index.tsx
+++ src/routes/signin/index.tsx
@@ -5,29 +5,29 @@ import { useAuthSession } from "../plugin@auth";
 import { type DocumentHead, useNavigate } from "@builder.io/qwik-city";
 
 export default component$(() => {
-  const session = useAuthSession();
-  const nav = useNavigate();
+    const session = useAuthSession();
+    const nav = useNavigate();
 
-  useOnDocument(
-    "load",
-    $(async () => {
-      if (session.value?.user) await nav("/");
-    })
-  );
-  return (
-    <>
-      <div class="flex flex-col items-stretch container mx-auto p-4 ">
-        <SigninForm provider="google">
+    useOnDocument(
+        "load",
+        $(async () => {
+            if (session.value?.user) await nav("/");
+        })
+    );
+    return (
+        <>
+            <div class="flex flex-col items-stretch container mx-auto p-4 ">
+                {/* <SigninForm provider="google">
           <BsGoogle class="ml-2" />
-        </SigninForm>
-        <SigninForm provider="github">
-          <BsGithub class="ml-2" />
-        </SigninForm>
-      </div>
-    </>
-  );
+        </SigninForm> */}
+                <SigninForm provider="github">
+                    <BsGithub class="ml-2" />
+                </SigninForm>
+            </div>
+        </>
+    );
 });
 
 export const head: DocumentHead = {
-  title: "Sign in",
+    title: "Sign in",
 };
